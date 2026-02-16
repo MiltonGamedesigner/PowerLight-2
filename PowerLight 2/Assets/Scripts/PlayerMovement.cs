6f1c66b22c701b28f3b272f4a9d8ecbdf3af3330
MiltonGamedesigner <miltonsund@gmail.com>
2026-02-16 16:02:51 +0100

    Player Movement Start

---
+++ PowerLight 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 move;
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    public void movePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+    }
+}
