61e77b6b33cb57e05fbc36e8ff7d08d1ab55898f
MiltonGamedesigner <miltonsund@gmail.com>
2026-02-24 14:40:36 +0100

    Interaction stuff påbörjat

---
+++ PowerLight 2/Assets/Scripts/Interactable.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public void Interact()
+    {
+
+
+
+
+
+    }
+
+}
--- PowerLight 2/Assets/Scripts/Interactor.cs
+++ PowerLight 2/Assets/Scripts/Interactor.cs
@@ -1,31 +1,29 @@
 using UnityEngine;
 
-interface IInteractable
-{
-    public void Interact();
-}
+
 public class Interactor : MonoBehaviour
 {
+    public int[] test;
+
     public LayerMask interactable;
     public Transform interactorSource;
     public float InteractRange;
 
     void Start()
     {
-
+        test[2] = 3;
     }
 
     void Update()
     {
+        int interactID;
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (Physics.OverlapSphere(interactorSource.position, 5f, interactable).Length > 0)
             {
-                Physics.OverlapSphere(interactorSource.position, 5f, interactable)[0].GetComponent<Transform>().position = Vector3.zero;
-
+                Physics.OverlapSphere(interactorSource.position, 5f, interactable)[0].GetComponent<Interactable>().Interact();
             }
         }
-
     }
 
 
