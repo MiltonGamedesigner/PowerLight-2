fe686cfbb718a1f84668171235329c330e47a6b0
MiltonGamedesigner <miltonsund@gmail.com>
2026-02-25 12:19:44 +0100

    Lite cinemachine camera work, bytte ut kameran och försöker programera en "cutscene"

--- PowerLight 2/Assets/Scripts/Interactable.cs
+++ PowerLight 2/Assets/Scripts/Interactable.cs
@@ -1,11 +1,16 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    public GameObject Object;
+    public bool On;
+    public string aniName;
+    public CinemachineCamera Camera;
 
     public void Interact()
     {
-
+        Camera.Priority = 10;
 
 
 
