d66d5484d1b2f463872d0a84002440a17912ecdb
dr4k0b <jakob.liljenstrom@gmail.com>
2026-05-07 11:43:06 +0200

    wrench

--- PowerLight 2/Assets/Scripts/wrench.cs
+++ PowerLight 2/Assets/Scripts/wrench.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class wrench : MonoBehaviour
 {
     private PlayerAttack player;
+    public float pickUpDistance;
     void Start()
     {
         player = FindFirstObjectByType<PlayerAttack>();
     }
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) <= 2 && Input.GetKeyDown(KeyCode.L))
+        if (Vector3.Distance(transform.position, player.transform.position) <= pickUpDistance && Input.GetKeyDown(KeyCode.L))
         {
             player.HasWrench = true;
             gameObject.SetActive(false);
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, pickUpDistance);
     }
 }
