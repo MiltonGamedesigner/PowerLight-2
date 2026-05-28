ad0e8e9459e5ba169d7c58b190c9844d77d499ee
MiltonGamedesigner <miltonsund@gmail.com>
2026-05-28 15:55:11 +0200

    git

---
+++ PowerLight 2/Assets/Scripts/Ending.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+        SceneManager.LoadScene(1);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
