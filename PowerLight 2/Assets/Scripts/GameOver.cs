6adc1ecc41c6a974d7ea2297a39f36625ca9bf04
dr4k0b <jakob.liljenstrom@gmail.com>
2026-05-28 15:58:11 +0200

    git

--- PowerLight 2/Assets/Scripts/GameOver.cs
+++ PowerLight 2/Assets/Scripts/GameOver.cs
@@ -21,7 +21,7 @@ public class GameOver : MonoBehaviour
     {
         if (canRestart && Input.GetKeyDown(KeyCode.L))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(0);
         }
     }
 }
